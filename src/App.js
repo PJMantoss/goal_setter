ab82ea8415e9d578122e89a00f066eaa5191bb7d
PJMantoss <joelptoss@gmail.com>
2021-07-25 10:28:39 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 function App() {
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
+  ])
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
