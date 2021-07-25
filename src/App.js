304be3039d07a2514177b4938ea0fe400505c850
PJMantoss <joelptoss@gmail.com>
2021-07-25 10:37:24 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
     { id: 'cg1', text: 'Finish the Course' },
     { id: 'cg2', text: 'Learn all about the Course Main Topic' },
     { id: 'cg3', text: 'Help other students in the Course Q&A' }
-  ])
+  ]);
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal />
+      
       <GoalList goals={courseGoals} />
     </div>
   );
